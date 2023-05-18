81e29452f1dc002cddda92f54c4578bc3785d69b
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-05-18 09:12:59 +0300

    Update AB.java

--- src/main/java/lesson_3/homework/AB.java
+++ src/main/java/lesson_3/homework/AB.java
@@ -3,13 +3,15 @@ package lesson_3.homework;
 Task 3
 Составьте программу, вычисляющую A*B, не пользуясь операцией умножения.
  */
+//+
 public class AB {
     public static void main(String[] args) {
         int a = 0;
         int b = 4;
         for (int i = 0; i < b; i++) {
+            //почему +3?
             a += 3;
         }
-        System.out.println("A = " +a);
+        System.out.println("A = " +a);//с твоими числами результат 12, хотя 0*4=0
     }
 }
