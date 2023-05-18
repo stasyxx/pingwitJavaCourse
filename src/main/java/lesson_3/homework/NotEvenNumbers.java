63d9274dfb4a86c2208edda17e98160dc7fe9eb7
stasyxx <anastasia.melnik.melnik@gmail.com>
2023-05-18 20:10:55 +0200

    les_4.homework

--- src/main/java/lesson_3/homework/NotEvenNumbers.java
+++ src/main/java/lesson_3/homework/NotEvenNumbers.java
@@ -1,18 +1,18 @@
 package lesson_3.homework;
+
 /*
 Task 6
 Напишите программу, определяющую сумму всех нечетных чисел от 1 до 99
  */
 public class NotEvenNumbers {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Сумма нечетных чисел от 1 до 99:");
         int sum = 0;
-        for ( int number = 0; number <= 99; number++) {
+        for (int number = 0; number <= 99; number++) {
             if (number % 2 != 0) {
                 sum += number;
             }
         }
-        //ctrl+alt+l
-        System.out.println ( sum );
+        System.out.println(sum);
     }
 }
---
+++ src/main/java/lesson_4/homework/Sum.java
@@ -0,0 +1,46 @@
+package lesson_4.homework;
+
+import java.util.Scanner;
+
+/*
+Task 1
+Вычислить: 1+2+4+8+...+256
+ */
+public class Sum {
+    public static void main(String[] args) {
+        int limitPower = 8;
+        int maxPower = getMaxPower(limitPower);
+        printSum(maxPower, limitPower);
+    }
+    public static void printSum(int maxPower, int limitPower) {
+        if (maxPower <= limitPower && maxPower >0 ) {
+
+            int i = 1;
+            int sum = 0;
+            double maxNumber = Math.pow(2, maxPower);
+            while (i <= maxNumber) {
+                sum += i;
+                i *= 2;
+            }
+            System.out.println("Sum of numbers is " + sum);
+        } else {
+            System.out.println("Ooops....500er500");
+        }
+ }
+ public static int getMaxPower(int limitPower) {
+     Scanner scan = new Scanner(System.in);
+     System.out.println("Enter power of two, but less than " + limitPower);
+     int maxPower = scan.nextInt();
+     return maxPower;
+ }
+ public static int calculateSum(int maxPower) {
+     int i = 1;
+     int sum = 0;
+     double maxNumber = Math.pow(2, maxPower);
+     while (i <= maxNumber) {
+         sum += i;
+         i *= 2;
+     }
+     return sum;
+}
+}
