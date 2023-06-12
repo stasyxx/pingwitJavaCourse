2053096ee4605555c771296377b22868027e207c
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-06-12 15:07:13 +0300

    Update Engine.java

--- src/main/java/lesson_9/homework/task3/Engine.java
+++ src/main/java/lesson_9/homework/task3/Engine.java
@@ -1,6 +1,7 @@
 package lesson_9.homework.task3;
-
+//Подразумевалось, что это полноценный класс с полями, конструктором
 public class Engine {
+    //что значит 'boolean engine'? Может isEngineOn? У тебя метод возвращает String в сигнатуре, а логика void.
     private String turnOnEngine(boolean engine) {
         if (engine = true) {
             System.out.println("You can't turn On the engine, because it is already On");
@@ -8,6 +9,7 @@ public class Engine {
             System.out.println("Turning on the engine");
         };
     };
+    //что значит 'boolean engine'? У тебя метод возвращает String в сигнатуре, а логика void.
     private String turnOffEngine(boolean engine) {
         if (engine = false) {
                 System.out.println("You can't turn Off the engine, because it is already Off");
