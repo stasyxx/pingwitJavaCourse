68722b068f87d46d6a37bb6294cbeb8fbcbc65aa
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-06-19 08:37:48 +0300

    Update SourCream.java

--- src/main/java/lesson_10/homework/SourCream.java
+++ src/main/java/lesson_10/homework/SourCream.java
@@ -1,5 +1,6 @@
 package lesson_10.homework;
 
+//разве сметана - овощ?
 public class SourCream extends Vegetable{
     public SourCream(double calories, double weight) {
         super(calories, weight);
