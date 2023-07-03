5029157b958dbe69a26b28ffcd1de4cc52c4f4b9
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-07-03 16:45:36 +0300

    Update LegoColorMain.java

--- src/main/java/lesson_14/homework/task_1/LegoColorMain.java
+++ src/main/java/lesson_14/homework/task_1/LegoColorMain.java
@@ -1,7 +1,7 @@
 package lesson_14.homework.task_1;
 
 import lesson_14.classwork.point_1.*;
-
+//+
 public class LegoColorMain {
     public static void main(String[] args) throws IllegalAccessException {
         Lego lego = new Lego("something", 5, LegoColor.BLUE);
