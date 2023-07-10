87aedab3f1988e663da8a103b88d2a97ae7932f9
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-07-10 08:53:01 +0300

    Update TextFromSecondTask.java

--- src/main/java/lesson_16/homework/task_3/TextFromSecondTask.java
+++ src/main/java/lesson_16/homework/task_3/TextFromSecondTask.java
@@ -8,8 +8,9 @@ import java.io.IOException;
 Task 3
 Прочитать файл из Task_2 и вывести на экран содержимое
  */
+//+, форматируй!
 public class TextFromSecondTask {
-    private static final String FILE_PATH = "src/main/java/lesson_16/homework/task_2/Text1000times.txt";
+    private static final String FILE_PATH = "src/main/java/lesson_16/homework/task_2/Text1000times.txt";//название файла с маленькой буквы
     public static void main(String[] args) {
         try (FileInputStream fis = new FileInputStream(FILE_PATH); BufferedInputStream bis = new BufferedInputStream(fis)) {
             int data;
