e6c9b939aed7e7a5426ee550465793b2c7388c89
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-07-10 08:52:26 +0300

    Update TextOutput.java

--- src/main/java/lesson_16/homework/task_2/TextOutput.java
+++ src/main/java/lesson_16/homework/task_2/TextOutput.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 Task 2
 Есть некоторый текст, его нужно записать в файл. Можно использовать одно и то же предложение, запишите его 1000 раз
  */
+//+
 public class TextOutput {
     private static final String FILE_PATH = ("src/main/java/lesson_16/homework/task_2/Text1000times.txt");
 
