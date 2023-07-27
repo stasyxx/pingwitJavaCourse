13fd3cc05b475d241d1ee9e0adbd7039f2a31d86
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-07-27 09:41:14 +0300

    Update Student.java

--- src/main/java/lesson_21/homework/task_2/Student.java
+++ src/main/java/lesson_21/homework/task_2/Student.java
@@ -2,7 +2,6 @@ package lesson_21.homework.task_2;
 
 public record Student(String name, String surname, int age) implements Comparable {
 
-
     @Override
     public boolean equals(Object obj) {
         return false;
@@ -19,8 +18,11 @@ public record Student(String name, String surname, int age) implements Comparabl
     }
 // не до конца разобралась с поочередностью и условиями сортировки вот здесь
     @Override
-    public int compareTo(Object students) {
+    public int compareTo(Object students) {//student - один
+        //list - плохое название переменной типа Student
         Student list = (Student) students;
+        //сначала можно по имени сравнить, потом фамилии, потом возврасту
+        //age - плохое название для переменной, отображающей результат сравнения имен
         int age = name.compareTo(list.name());
         if (age != 0) {
             return age;
