66940bdb523b970bc1bd2e5de59a44e001c0db89
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-08-14 12:21:28 +0300

    Update SumNumbers.java

--- src/main/java/lesson_24/homework/task_2/SumNumbers.java
+++ src/main/java/lesson_24/homework/task_2/SumNumbers.java
@@ -6,7 +6,7 @@ Task 2 (limit, reduce)
 
 import java.util.Arrays;
 import java.util.List;
-//+
+//ты нашла сумму, а не произвведение :)
 public class SumNumbers {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
